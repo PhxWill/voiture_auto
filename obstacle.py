4fb6c3f9f6fd49e6d754ded1441c7a41201e8822
Tisdag <49315267+Tisdag@users.noreply.github.com>
2022-03-04 13:36:52 +0100

    Add files via upload
    Faire attention aux différentes version de python (2/3)

---
+++ obstacle.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+import rospy
+
+
+from sensor_msgs.msg import LaserScan
+
+
+class ObstacleAvoidance():
+
+    def __init__(self):
+        
+        rospy.init_node('obstacle')
+        
+        rospy.Subscriber('scan', LaserScan, self.scan)
+
+        
+        self.PositionEnd = [-4, 46.020]
+        self.error = 1
+        self.stopped = False
+        
+        rospy.spin()
+
+
+    def scan(self, laser):
+        
+
+        state_area = ''
+    
+        area = {
+            'right':  min(min(laser.ranges[200:431]), 10),
+            'front':  min(min(laser.ranges[432:648]), 10),
+            'left':   min(min(laser.ranges[649:880]), 10),
+        }
+        
+        if not self.stopped:
+        
+            if area['front'] > 0.2 and area['left'] > 0.2 and area['right'] > 0.2:
+                state_area = 'case 1 - no obstacle'
+                
+            elif area['front'] < 0.2 and area['left'] < 0.2 and area['right'] > 0.2:
+                state_area = 'case 2 - obstacle in front/left'
+                
+            elif area['front'] < 0.2 and area['left'] > 0.2 and area['right'] < 0.2:
+                state_area = 'case 3 - obstacle in front/right'
+                
+            elif area['front'] > 0.2 and area['left'] > 0.2 and area['right'] < 0.2:
+                state_area = 'case 4 - obstacle in right'
+                
+            elif area['front'] > 0.2 and area['left'] < 0.2 and area['right'] > 0.2:
+                state_area = 'case 5 - obstacle in left'
+                
+            elif area['front'] < 0.2 and area['left'] < 0.2 and area['right'] < 0.2:
+                state_area = 'case 6 - obstacle in front and left and right'
+                
+                 
+            else:
+                state_area = 'unknown case'
+                
+                rospy.loginfo(area)
+
+            rospy.loginfo(state_area)
+            
+                 
+        else:
+            state_area = 'case 7 - It is finihed !'
+            
+            rospy.loginfo(state_area)
+            
+                       
+
+    
+            
+            
+            
+if __name__ == '__main__':
+    OA = ObstacleAvoidance()
